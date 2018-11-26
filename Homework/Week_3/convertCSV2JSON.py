6ec8e041bfaf2efd79d827a849d4345970efecd5
tjmf94 <thomasfranx@hotmail.com>
2018-11-26 23:38:01 +0100

    Assignment week 3

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -2,17 +2,26 @@ import csv
 import json
 
 
-def convert(input_file, variables, data_start, data_end):
+def convert(input_file, sort_by, header_line, last_data_line):
+
     with open(input_file, "r") as input_file:
-        with open("output.json", "w") as output_file:
-            input = csv.DictReader(input_file, variables)
-            for index, row in enumerate(input):
-                if index >= data_start and index <= data_end:
-                    row["periode"] = int(row["periode"].split()[0])
-                    json.dump(row, output_file)
-                    output_file.write('\n')
+        with open("cleaned_data.csv", "w") as cleaned_file:
+
+            for index, line in enumerate(input_file):
+                if index >= (header_line - 1) and index <= (last_data_line - 1):
+                    cleaned_file.write(line)
+
+    with open("cleaned_data.csv", "r") as cleaned_file:
+        input = csv.DictReader(cleaned_file)
+
+        data = {}
+        for row in input:
+            key = "jaar"+row[sort_by].split()[0]
+            data[key] = row
+
+    with open("output.json", "w") as output_file:
+        output_file.write(json.dumps(data, indent=4))
 
 
 if __name__ == "__main__":
-    convert("levensverwachting.csv", ("periode", "levensverwachting mannen",
-                                      "levensverwachting vrouwen"), 5, 12)
+    convert("levensverwachting.csv", "Perioden", 4, 12)
